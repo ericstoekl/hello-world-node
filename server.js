1c1b6c8f9bee9b635e1052b83392befb331387f8
Eric Stoekl <ems5311@gmail.com>
2018-01-18 23:25:09 -0800

    hello my friends

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Friends!!\n');
+  res.send('Hello my Friends!!\n');
 });
 
 app.listen(PORT, HOST);
